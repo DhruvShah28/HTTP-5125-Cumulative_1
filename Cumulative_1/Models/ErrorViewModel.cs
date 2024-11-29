6aba228bfbc666a0912a6632701bbeeb4b7e32f6
dhruvshah28 <dhruvshah2811ds@gmail.com>
2024-11-29 16:06:20 -0500

    issue

--- Cumulative_1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cumulative_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
