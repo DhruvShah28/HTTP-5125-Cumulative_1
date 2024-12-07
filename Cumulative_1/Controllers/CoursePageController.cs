53773670c7c9c14686d033a2fcd53f267c85a33b
dhruvshah28 <dhruvshah2811ds@gmail.com>
2024-12-07 02:27:38 -0500

    cumulative 3 here

---
+++ Cumulative_1/Controllers/CoursePageController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cumulative_1.Models;
+
+namespace Cumulative_1.Controllers
+{
+    public class CoursePageController : Controller
+    {
+        // API handles gathering all the data from
+        // the Database and MVC is responsible for creating an HTTP response
+        // and showing it on a web page that displays the data from database
+        // to the View.
+
+        private readonly CourseAPIController _api;
+
+        public CoursePageController(CourseAPIController api)
+        {
+            _api = api;
+        }
+
+        public IActionResult CList()
+        {
+            List<Course> courses = _api.Lcourses();
+
+            return View(courses);
+        }
+
+        public IActionResult CShow(int id)
+        {
+            Course SelCourse = _api.CourseInfo(id);
+            return View(SelCourse);
+        }
+
+        // GET : CoursePage/NewCourse
+        [HttpGet]
+        public IActionResult C_New(int id)
+        {
+            return View();
+        }
+
+
+        // POST: CoursePage/CreateCourse
+        [HttpPost]
+        public IActionResult CreateCourse(Course NewCourse)
+        {
+            int CourseId = _api.AddCourse(NewCourse);
+
+            // redirects to "Show" action on "Course" cotroller with id parameter supplied
+            return RedirectToAction("CShow", new { id = CourseId });
+        }
+
+        // GET : CoursePage/DeleteConfirmCourse/{id}
+        [HttpGet]
+        public IActionResult C_DeleteConfirm(int id)
+        {
+            Course SelectedCourse = _api.CourseInfo(id);
+            return View(SelectedCourse);
+        }
+
+
+        // POST: CoursePage/DeleteCourse/{id}
+        [HttpPost]
+        public IActionResult DeleteCourse(int id)
+        {
+            int CourseId = _api.DeleteCourse(id);
+            // redirects to list action
+            return RedirectToAction("CList");
+        }
+
+        // GET : CoursePage/EditCourse/{id}
+        [HttpGet]
+        public IActionResult C_Edit(int id)
+        {
+            Course SelectedCourse = _api.CourseInfo(id);
+            return View(SelectedCourse);
+        }
+
+
+
+        // POST: CoursePage/Update/{id}
+        [HttpPost]
+        public IActionResult Update(int id, string Course_Code, DateTime C_Start_Date, DateTime C_End_Date, string Course_Name, int Teacher_Id)
+        {
+            Course UpdatedCourse = new Course();
+            UpdatedCourse.Course_Code = Course_Code;
+            UpdatedCourse.C_Start_Date = C_Start_Date;
+            UpdatedCourse.C_End_Date = C_End_Date;
+            UpdatedCourse.Course_Name = Course_Name;
+            UpdatedCourse.Teacher_Id = Teacher_Id;
+
+
+            // not doing anything with the response
+            _api.UpdateCourse(id, UpdatedCourse);
+
+            // redirects to show teacher
+            return RedirectToAction("CShow", new { id });
+        }
+    }
+}
---
+++ Cumulative_1/Controllers/TeacherPageController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cumulative_1.Models;
+
+
+namespace Cumulative_1.Controllers
+{
+    public class TeacherPageController : Controller
+    {
+
+        // API handles gathering all the data from
+        // the Database and MVC is responsible for creating an HTTP response
+        // and showing it on a web page that displays the data from database
+        // to the View.
+
+        private readonly TeacherAPIController _api;
+
+        public TeacherPageController(TeacherAPIController api)
+        {
+            _api = api;
+        }
+
+
+        /// <summary>
+        /// The 4th link added to our web page layout.cshtml is to teachers page, 
+        /// it redirects to a cshtml apge that shows a list of all teachers in our created database.
+        /// </summary>
+        /// <example>
+        /// GET api/Teacher/LTeachers -> Alexander Bennett, Caitlin Cummings, Linda Chan, Lauren Smith, Jessica Morris......
+        /// </example>
+        /// <returns>
+        /// A list all the teachers from teachers table in the database school
+        /// </returns>
+
+        public IActionResult List()
+        {
+            List<Teacher> Teachers = _api.LTeachers();
+            return View(Teachers);
+        }
+
+
+
+        /// <summary>
+        /// When we click on a teacher name, it redirects to a new webpage that shows details of that teacher
+        /// </summary>
+        /// <remarks>
+        /// it will select the ID of the teacher when you click on its name and it selects the data from the database from the selected id
+        /// </remarks>
+        /// <example>
+        /// GET api/Teacher/TeacherInfo/3 -> {"TeacherId":3,"TeacherFname":"Caitlin","TeacherLName":"Cummings", "Employee Number" : "T381", "Hire Date" : "2014-6-10", "Salary" : "62.77"}
+        /// </example>
+        /// <returns>
+        /// list of all Information about the Selected Teacher from their database
+        /// </returns>
+        public IActionResult Show(int id)
+        {
+            Teacher SelTeachers = _api.TeacherInfo(id);
+            return View(SelTeachers);
+        }
+
+        // GET : TeacherPage/New
+        [HttpGet]
+        public IActionResult New(int id)
+        {
+            return View();
+        }
+
+        // POST: TeacherPage/Create
+        [HttpPost]
+        public IActionResult Create(Teacher NewTeacher)
+        {
+            int TeacherId = _api.AddTeacher(NewTeacher);
+
+            // redirects to "Show" action on "Teacher" cotroller with id parameter supplied
+            return RedirectToAction("Show", new { id = TeacherId });
+        }
+
+
+
+        // GET : TeacherPage/DeleteConfirm/{id}
+        [HttpGet]
+        public IActionResult DeleteConfirm(int id)
+        {
+            Teacher SelectedTeacher = _api.TeacherInfo(id);
+            return View(SelectedTeacher);
+        }
+
+
+        // POST: TeacherPage/Delete/{id}
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            int TeacherId = _api.DeleteTeacher(id);
+            // redirects to list action
+            return RedirectToAction("List");
+        }
+
+        // GET : TeacherPage/Edit/{id}
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Teacher SelectedTeacher = _api.TeacherInfo(id);
+            return View(SelectedTeacher);
+        }
+
+
+
+        // POST: TeacherPage/Update/{id}
+        [HttpPost]
+        public IActionResult Update(int id, string First_Name, string Last_Name, decimal Salary, string Emp_num, DateTime HireDate)
+        {
+            Teacher UpdatedTeacher = new Teacher();
+            UpdatedTeacher.First_Name = First_Name;
+            UpdatedTeacher.Last_Name = Last_Name;
+            UpdatedTeacher.Salary = Salary;
+            UpdatedTeacher.Emp_Num = Emp_num;
+            UpdatedTeacher.HireDate = HireDate;
+
+
+            // not doing anything with the response
+            _api.UpdateTeacher(id, UpdatedTeacher);
+
+
+            // redirects to show teacher
+            return RedirectToAction("Show", new { id });
+        }
+
+    }
+}
